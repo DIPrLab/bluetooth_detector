521d55053ab26b948168dbd8773a0c6a288b30c5
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-08-27 16:03:40 -0700

    Fixed Broken JSON Parsing

--- lib/report/device.g.dart
+++ lib/report/device.g.dart
@@ -10,14 +10,13 @@ Device _$DeviceFromJson(Map<String, dynamic> json) => Device(
       json['id'] as String,
       json['name'] as String,
       json['platformName'] as String,
-      (json['manufacturer'] as List<dynamic>)
-          .map((e) => (e as num).toInt())
-          .toList(),
-    );
+      (json['manufacturer'] as List<dynamic>).map((e) => (e as num).toInt()).toList(),
+    )..dataPoints = (json['dataPoints'] as List<dynamic>).map((e) => Datum.fromJson(e as Map<String, dynamic>)).toSet();
 
 Map<String, dynamic> _$DeviceToJson(Device instance) => <String, dynamic>{
       "\"id\"": "\"${instance.id}\"",
       "\"name\"": "\"${instance.name}\"",
       "\"platformName\"": "\"${instance.platformName}\"",
       "\"manufacturer\"": instance.manufacturer,
+      "\"dataPoints\"": instance.dataPoints.map((datum) => "${datum.toJson()}").toList(),
     };
--- lib/report/report.g.dart
+++ lib/report/report.g.dart
@@ -8,12 +8,11 @@ part of 'report.dart';
 
 Report _$ReportFromJson(Map<String, dynamic> json) => Report(
       (json['report'] as Map<String, dynamic>).map(
-        (k, e) => MapEntry(
-            k, e == null ? null : Device.fromJson(e as Map<String, dynamic>)),
+        (k, e) => MapEntry(k, e == null ? null : Device.fromJson(e as Map<String, dynamic>)),
       ),
-    );
+    )..time = DateTime.parse(json['time'] as String);
 
 Map<String, dynamic> _$ReportToJson(Report instance) => <String, dynamic>{
-      'report': instance.report
-          .map((a, b) => MapEntry("${a}", "${b?.toJson() ?? ""}")),
+      "\"time\"": "\"${instance.time.toIso8601String()}\"",
+      "\"report\"": instance.report.map((a, b) => MapEntry("\"${a}\"", "${b?.toJson() ?? ""}")),
     };
