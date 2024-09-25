20d4e58f4fcc87e3c40e8e7764cd380b37d0fd5f
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-09-24 18:28:56 -0700

    Added OrderedPairs and mapOrderedPairs Methods

---
+++ lib/extensions/ordered_pairs.dart
@@ -0,0 +1,13 @@
+extension OrderedPairs<T, E> on List<T> {
+  List<(T, T)> orderedPairs() {
+    List<(T, T)> result = List.empty(growable: true);
+    for (int i = 0; i < this.length - 1; i++) {
+      T t1 = this[i];
+      T t2 = this[i + 1];
+      result.add((t1, t2));
+    }
+    return result;
+  }
+
+  List<E> mapOrderedPairs(E Function((T, T)) toElement) => this.orderedPairs().map(toElement).toList();
+}
