96c66d1b7f758804e35384a2014ffc4c89c47dd4
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2024-10-06 22:30:57 -0700

    Remove LocationButton; Removed Unnecessary SetStates; Simplified Syntax

--- lib/scanner_view/buttons.dart
+++ lib/scanner_view/buttons.dart
@@ -1,105 +1,35 @@
 part of 'package:bluetooth_detector/scanner_view/scanner_view.dart';
 
 extension Buttons on ScannerViewState {
-  Widget autoConnectButton() {
-    return FloatingActionButton.large(
-      onPressed: () async {
-        autoConnect = !autoConnect;
-        print("Auto Connect: ${autoConnect}");
-        setState(() {});
+  Widget reportViewerButton() => FloatingActionButton.large(
+      onPressed: () {
+        log();
+        widget.report.refreshCache(widget.settings);
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => SafeArea(child: ReportView(widget.settings, report: widget.report))));
       },
-      child: Icon(
-        autoConnect ? Icons.bluetooth : Icons.bluetooth_disabled,
-      ),
-    );
-  }
-
-  Widget locationButton() {
-    if (location == null) {
-      return FloatingActionButton.large(
-        onPressed: () async {
-          enableLocationStream();
-          location = await getLocation();
-          print("Enabling Location Stream");
-          setState(() {});
-        },
-        child: const Icon(
-          Icons.location_disabled,
-        ),
-      );
-    } else {
-      return FloatingActionButton.large(
-        onPressed: () {
-          disableLocationStream();
-          location = null;
-          print("Disabling Location Stream");
-          setState(() {});
-        },
-        child: const Icon(
-          Icons.location_searching,
-        ),
-      );
-    }
-  }
+      child: const Icon(Icons.newspaper));
 
   Widget scanButton() {
     if (FlutterBluePlus.isScanningNow) {
       return FloatingActionButton.large(
-        onPressed: () {
-          log();
-          stopScan();
-          write(widget.report);
-          Vibration.vibrate(pattern: [
-            250,
-            100,
-            100,
-            100,
-            100,
-            100,
-            250,
-            100,
-            500,
-            250,
-            250,
-            100,
-            750,
-            500
-          ], intensities: [
-            255,
-            0,
-            255,
-            0,
-            255,
-            0,
-            255,
-            0,
-            255,
-            0,
-            255,
-            0,
-            255,
-            0
-          ]);
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => SafeArea(
-                      child:
-                          ReportView(widget.settings, report: widget.report))));
-        },
-        child: const Icon(
-          Icons.stop,
-        ),
-      );
+          onPressed: () {
+            log();
+            stopScan();
+            write(widget.report);
+            Vibration.vibrate(
+                pattern: [250, 100, 100, 100, 100, 100, 250, 100, 500, 250, 250, 100, 750, 500],
+                intensities: [255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0]);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SafeArea(child: ReportView(widget.settings, report: widget.report))));
+          },
+          child: const Icon(Icons.stop));
     } else {
-      return FloatingActionButton.large(
-        onPressed: () {
-          startScan();
-        },
-        child: const Icon(
-          Icons.play_arrow_rounded,
-        ),
-      );
+      return FloatingActionButton.large(onPressed: () => startScan(), child: const Icon(Icons.play_arrow_rounded));
     }
   }
 
@@ -108,14 +38,8 @@ extension Buttons on ScannerViewState {
       onPressed: () {
         Navigator.push(
             context,
-            MaterialPageRoute(
-                builder: (context) =>
-                    SafeArea(child: SettingsView(widget.settings))));
-        setState(() {});
-      },
-      child: Icon(
-        Icons.settings,
-      ),
-    );
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SafeArea(child: SettingsView(widget.settings)))),
+        child: const Icon(Icons.settings));
   }
 }
